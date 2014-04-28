5155d3334a5406df6944d6787a45eb5c2d67f38b
Marcus Battle <marcus@marcusbattle.com>
2014-04-28 10:56:06 -0400

    field updates

--- templates/product-field-settings.php
+++ templates/product-field-settings.php
@@ -1,8 +1,22 @@
-<?php
-	
-	
-?>
-
-<div id="product-settings">
-
+<div class="custom-setting" data-title="Product Settings" data-field-type="product">
+	<ul>
+		<li>
+		   	<label>Products</label>
+		   	<select id="product_id" multiple style="width: 100%;">
+		   		<?php foreach( lbox_get_products() as $product ): ?>
+		  			<option value="<?php echo $product->ID ?>"><?php echo $product->post_title ?></option>
+				<?php endforeach; ?>	  
+			</select>
+			<small>To select more than one product hold down control (command on Mac) and click</small>
+		</li>
+	</ul>
+	<script>
+		(function($){
+			$('.custom-setting #product_id').on('click', function(){
+				if ( $('#lbox-form-fields > div.focus').data('field-type') == 'product' ) {
+					alert('editing a product');
+				}
+			});
+		})(jQuery);
+	</script>
 </div>
--- templates/product-field.php
+++ templates/product-field.php
@@ -1,9 +1,8 @@
 <button>Product
 	<div class="form-group" data-field-type="product">
 		<label>Product</label>
-		<select name="field[product]">
+		<select id="fields[product_id]">
 			<option>--</option>
 		</select>
-		<input type="hidden" name="actions[buy_product]" value="" />
 	</div>
 </button>
