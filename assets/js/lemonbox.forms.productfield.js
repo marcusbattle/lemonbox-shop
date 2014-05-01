059a0b6b4549bce9b7d92b75743b9dbf731ddceb
Marcus Battle <marcus@marcusbattle.com>
2014-05-01 09:34:16 -0400

    credit card support

--- assets/js/lemonbox.forms.productfield.js
+++ assets/js/lemonbox.forms.productfield.js
@@ -3,16 +3,16 @@
 	$('select[name="fields[product_id][]"]').on( 'change', function(){
 
 		var selected = $(this).find('option:selected');
-		var payment_type = selected.data('payment-type');
+		var payment_type = (selected.data('payment-type')) ? selected.data('payment-type') : null ;
 		var amount_field = selected.closest('.field').find('input[name="fields[amount][]"]');
 
-		if( payment_type && ( payment_type != 'free' ) ) {
+		if ( payment_type == 'fixed' ) {
 
-			amount_field.val('$100');
+			amount_field.val('100');
 
 		} else {
-			
-			amount_field.val('$1000');
+
+			amount_field.val('0');
 
 		}
 
--- templates/product-field.php
+++ templates/product-field.php
@@ -2,13 +2,64 @@
 	<div class="field form-group" data-field-type="product" data-disable-name="true" data-disable-placeholder="true">
 		<label>Product</label>
 		<div class="row">
-			<div class="col-md-8">
+			<div class="col-xs-8 col-sm-8 col-md-8">
 				<select name="fields[product_id][]" class="form-control">
 					<option value="">--</option>
 				</select>
 			</div>
-			<div class="col-md-4">
-				<input type="text" name="fields[amount][]" class="form-control" placeholder="$100" />
+			<div class="col-xs-4 col-sm-4 col-md-4">
+				<div class="input-group">
+					<span class="input-group-addon">$</span>
+					<input type="text" name="fields[amount][]" class="form-control" placeholder="00.00" />
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="field form-group" data-field-type="creditcard" data-disable-name="true" data-disable-placeholder="true">
+		<div class="row row-1">
+			<div class="col-xs-8 col-sm-8 col-md-8">
+				<label>Credit Card</label>
+				<input type="text" class="form-control" placeholder="**** **** **** 0000" />
+			</div>
+			<div class="col-xs-4 col-sm-4 col-md-4">
+				<label>Secuirty Code</label>
+				<input type="text" class="form-control" placeholder="•••" maxlength="3" />
+			</div>
+		</div>
+		<div class="row row-2">
+			<div class="col-xs-6 col-sm-6 col-md-6">
+				<label>Name on Card</label>
+				<input type="text" name="name_on_card" class="form-control" placeholder="John Doe" />
+			</div>
+			<div class="col-xs-6 col-sm-6 col-md-6">
+				<label>Expiration</label>
+				<div class="row">
+					<div class="col-xs-6 col-md-6">
+						<select name="fields[exp_month]" class="form-control">
+							<option value="">MM</option>
+							<option value="">01</option>
+							<option value="">02</option>
+							<option value="">03</option>
+							<option value="">04</option>
+							<option value="">05</option>
+							<option value="">06</option>
+							<option value="">07</option>
+							<option value="">08</option>
+							<option value="">09</option>
+							<option value="">10</option>
+							<option value="">11</option>
+							<option value="">12</option>
+						</select>
+					</div>
+					<div class="col-xs-6 col-md-6">
+						<select name="fields[exp_year]" class="form-control">
+							<option value="">YY</option>
+							<?php for( $y = 0; $y < 11; $y++ ): $year = date('y', strtotime('+' . $y . ' years' )); ?>
+							<option value="<?php echo $year ?>"><?php echo $year ?></option>
+							<?php endfor; ?>
+						</select>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
